853b71ecdc0f076237d53afa9cfb0e39c32ece76
AlysonDev <devalyson@gmail.com>
2023-02-16 00:41:33 -0300

    Adcionando botões do whatsapp e instagagram

--- src/components/Header/Nav/index.tsx
+++ src/components/Header/Nav/index.tsx
@@ -1,11 +1,59 @@
+import { useState } from "react";
+
 export function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="w-[25%]">
-      <ul className="flex flex-row justify-between">
-        <li className="hover:bg-red-400 duration-700 cursor-pointer p-2 rounded-md">Home</li>
-        <li className="hover:bg-red-400 duration-700 cursor-pointer p-2 rounded-md">Contato</li>
-        <li className="hover:bg-red-400 duration-700 cursor-pointer p-2 rounded-md">Dados</li>
-      </ul>
+    <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <span className="font-semibold text-xl tracking-tight">Meu Menu</span>
+      </div>
+      <div className="block lg:hidden">
+        <button
+          className="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white"
+          onClick={toggleMenu}
+        >
+          <svg
+            className="fill-current h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path
+              d="M0 3a3 3 0 013-3h14a3 3 0 013 3v1a3 3 0 01-3 3H3a3 3 0 01-3-3V3zm0 7a3 3 0 013-3h14a3 3 0 013 3v1a3 3 0 01-3 3H3a3 3 0 01-3-3v-1zm0 7a3 3 0 013-3h14a3 3 0 013 3v1a3 3 0 01-3 3H3a3 3 0 01-3-3v-1z"
+            />
+          </svg>
+        </button>
+      </div>
+      <div
+        className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto hidden overflow-hidden transition-all duration-500 ease-in-out ${isOpen ? 'max-h-full' : 'max-h-0'
+          }`}
+      >
+        <div className="text-sm lg:flex-grow">
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4"
+          >
+            Página Inicial
+          </a>
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4"
+          >
+            Sobre
+          </a>
+          <a
+            href="#responsive-header"
+            className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white"
+          >
+            Contato
+          </a>
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,46 @@
+import { RiInstagramFill, RiWhatsappFill } from "react-icons/ri";
 import logo from "../../assets/logo.webp";
 import { Nav } from "./Nav";
 
 export function Header() {
   return (
-    <header
-      className="flex 
+    <>
+      <header
+        className="flex 
       flex-row h-20 align-center justify-between 
       px-6 items-center" >
-      <picture className="cursor-pointer ">
-        <img src={logo} alt="logo marca do grupo IN9" className="w-12" />
-      </picture>
+        <div className="flex items-center">
+          <picture className="cursor-pointer ">
+            <img src={logo} alt="logo marca do grupo IN9" className="w-12" />
+          </picture>
+          <div className="flex flex-col text-center">
+            <h1 className="font-extrabold text-xl">
+              Grupo In9 Prestige
+            </h1>
+            <span className="text-[0.92rem] mt-[-0.4rem]">
+              Consultoria e Capacitação
+            </span>
+          </div>
+        </div>
+
+        <a
+          href="https://wa.me/+5585998476785"
+          target={"_blank"}
+          className="hidden md:flex items-center gap-2  hover:text-secondary-800 transition-all delay-50"
+        >
+          <RiWhatsappFill size={28} />
+          <span>+55 85 99847 6785</span>
+        </a>
+
+        <div className="flex items-center hover:text-secondary-800 transition-all delay-50">
+          <a href="www.instagram.com" target={"_blank"}>
+            <RiInstagramFill size={28} />
+          </a>
+
+        </div>
+
+      </header>
       <Nav />
-    </header>
+    </>
   )
 }
